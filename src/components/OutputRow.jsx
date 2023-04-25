1a781723c27d16d5ba7c6b0c74d439538144beb8
Lydia Dunning <lydia.dunning@gmail.com>
2023-04-25 16:19:37 -0700

    Modified OutputRow comment

--- src/components/OutputRow.jsx
+++ src/components/OutputRow.jsx
@@ -1,9 +1,11 @@
 const OutputRow = ({ output, field }) => {
   // OutputRow makes a worthwhile component because of its fiddly conditional details.
-  // Multiple changes occur depending on one value, so the hook for this component needs
+  // Multiple changes occur depending on one value, so this component needs
   // very few props, where equivalent jsx in the parent document would require several 
   // extra lines or a lengthy element definition.
+
   // This feels like one of those use-cases React was designed for.
+
   const textOutput = output[field] ? output[field] : '- -'
   const classes = output[field] ? 'purple' : 'purple no-output'
 
